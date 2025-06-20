3a52a051c53e5120fa45de7fb72c80d553d76518
David Guentelican <david.guentelican@gmail.com>
2025-06-20 02:42:25 -0400

    se crea ruta para crear usuario admin, sin validaciones

---
+++ prj_api_ecd_bibl/app_cuentas/serializers.py
@@ -0,0 +1,36 @@
+"""
+Aquí irán todos los serializers para crear rutas de app_cuentas
+"""
+
+from rest_framework import serializers
+from .models import Usuario
+
+
+#* SERIALIZERS POST
+
+#CREAR USUARIO
+#20/06/25
+
+class UsuarioCreateAdminSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Usuario
+        fields = [
+            'username',
+            'password',
+            'first_name',
+            'last_name',
+            'email',
+            'rut',
+            'telefono',
+            'rol',
+            'foto_perfil'
+        ]
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        usuario = Usuario(**validated_data)
+        usuario.set_password(password)
+        usuario.save()
+        return usuario
--- prj_api_ecd_bibl/app_cuentas/views.py
+++ prj_api_ecd_bibl/app_cuentas/views.py
@@ -1,6 +1,11 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework import status
+from .serializers import UsuarioCreateAdminSerializer
+from rest_framework.views import APIView
+from rest_framework import generics
+from .models import Usuario
 
 
 #* RUTA DE VALIDACION DE SALUD DE LA API
@@ -10,3 +15,15 @@ from rest_framework.permissions import AllowAny
 @permission_classes([AllowAny])
 def api_status(request):
     return Response({"message": "API ECD Cuentas disponible"})
+
+###############################################################################################
+
+#* RUTAS POST
+
+#RUTA PARA REGISTRAR USUARIO
+#20/06/25
+
+class CrearUsuarioAdminAPIView(generics.CreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioCreateAdminSerializer
+    permission_classes = [AllowAny]
