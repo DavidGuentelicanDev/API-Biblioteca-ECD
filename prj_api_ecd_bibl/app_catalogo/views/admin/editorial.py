58d23f385533b9175a63fe372305181173b26d83
David Guentelican <david.guentelican@gmail.com>
2025-06-25 23:33:06 -0400

    se cambian los metodos para las rutas, usando estandares de django rest framework para unificar

--- prj_api_ecd_bibl/app_catalogo/views/admin/editorial.py
+++ prj_api_ecd_bibl/app_catalogo/views/admin/editorial.py
@@ -4,7 +4,7 @@ RUTAS DE EDITORIAL
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics, status
 from ...models import Editorial
 from ...serializers.admin.editorial import EditorialSerializer
 from rest_framework.permissions import AllowAny
@@ -13,77 +13,65 @@ from rest_framework.permissions import AllowAny
 #* RUTA PARA LISTAR TODAS LAS EDITORIALES / CREAR EDITORIALES
 #25/06/25
 
-class EditorialListCreateAPIView(APIView):
+class EditorialListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Editorial.objects.all()
+    serializer_class = EditorialSerializer
     permission_classes = [AllowAny]
 
-    #método para ruta get (listar)
+    #método para ruta post (create)
     #25/06/25
-    def get(self, request):
-        editoriales = Editorial.objects.all()
-        serializer = EditorialSerializer(editoriales, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    #método para ruta post (crear)
-    #25/06/25
-    def post(self, request):
-        serializer = EditorialSerializer(data=request.data)
-
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            editorial = serializer.save()
+            return Response({
+                "status": "success",
+                "message": f"Editorial {str(editorial)} creada exitosamente.",
+                "editorial": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            "status": "error",
+            "message": "Error al crear la editorial.",
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
 
 ############################################################################################
 
 #* RUTA PARA FILTAR EDITORIAL POR ID / EDITAR EDITORIAL POR ID / ELIMINAR EDITORIAL POR ID
 #25/06/25
 
-class EditorialRetrieveUpdateDeleteAPIView(APIView):
+class EditorialRetrieveUpdateDeleteAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Editorial.objects.all()
+    serializer_class = EditorialSerializer
     permission_classes = [AllowAny]
 
-    #método para filtrar editorial por id
+    #método para ruta put (update)
     #25/06/25
-    def get_editorial(self, pk):
-        try:
-            return Editorial.objects.get(pk=pk)
-        except Editorial.DoesNotExist:
-            return None
-
-    #metodo para ruta get por id (listar)
-    #25/06/25
-    def get(self, request, pk):
-        editorial = self.get_editorial(pk)
-
-        if not editorial:
-            return Response({'detail': 'No encontrado.'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = EditorialSerializer(editorial)
-        return Response(serializer.data)
-
-    #método para ruta put (actualizar)
-    #25/06/25
-    def put(self, request, pk):
-        editorial = self.get_editorial(pk)
-
-        if not editorial:
-            return Response({'detail': 'No encontrado.'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = EditorialSerializer(editorial, data=request.data)
-
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        nombre = instance.nombre
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            editorial = serializer.save()
+            return Response({
+                "status": "success",
+                "message": f"Editorial {nombre} actualizada exitosamente.",
+                "editorial": serializer.data
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "status": "error",
+            "message": "Error al actualizar la editorial.",
+            "errors": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    #método para ruta delete (eliminar)
+    #método para ruta delete
     #25/06/25
-    def delete(self, request, pk):
-        editorial = self.get_editorial(pk)
-
-        if not editorial:
-            return Response({'detail': 'No encontrado.'}, status=status.HTTP_404_NOT_FOUND)
-
-        editorial.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        nombre = instance.nombre
+        self.perform_destroy(instance)
+        return Response({
+            "status": "success",
+            "message": f"Editorial {nombre} eliminada exitosamente."
+        }, status=status.HTTP_204_NO_CONTENT)
