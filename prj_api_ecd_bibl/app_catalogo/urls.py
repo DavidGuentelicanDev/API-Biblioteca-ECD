16f6f745dd03cf851c4adf6d3f854be75bc3f4b4
David Guentelican <david.guentelican@gmail.com>
2025-06-28 16:21:18 -0400

    versionado de rutas app_catalogo

--- prj_api_ecd_bibl/app_catalogo/urls.py
+++ prj_api_ecd_bibl/app_catalogo/urls.py
@@ -8,29 +8,29 @@ from .views.web.libro import LibroListAPIView, LibroRetrieveAPIView
 
 urlpatterns = [
     #* STATUS
-    path('status/', status.api_status, name='api_status'),
+    path('v1/status/', status.api_status, name='api_status'),
 
     #* ADMIN
     #EDITORIAL
     #listar y crear editoriales
-    path('admin/editoriales/', EditorialListCreateAPIView.as_view(), name='admin_editoriales_listar_crear'),
+    path('v1/admin/editoriales/', EditorialListCreateAPIView.as_view(), name='admin_editoriales_listar_crear'),
     #listar editorial por id | actualizar editorial | eliminar editorial
-    path('admin/editoriales/<int:pk>/', EditorialRetrieveUpdateDeleteAPIView.as_view(), name='admin_editoriales_detalle_editar_eliminar'),
+    path('v1/admin/editoriales/<int:pk>/', EditorialRetrieveUpdateDeleteAPIView.as_view(), name='admin_editoriales_detalle_editar_eliminar'),
     #AUTOR
     #listar y crear autores
-    path('admin/autores/', AutorListCreateAPIView.as_view(), name='admin_autores_listar_crear'),
+    path('v1/admin/autores/', AutorListCreateAPIView.as_view(), name='admin_autores_listar_crear'),
     #listar autor por id | actualizar autor | eliminar autor
-    path('admin/autores/<int:pk>/', AutorRetrieveUpdateDeleteAPIView.as_view(), name='admin_autores_detalle_editar_eliminar'),
+    path('v1/admin/autores/<int:pk>/', AutorRetrieveUpdateDeleteAPIView.as_view(), name='admin_autores_detalle_editar_eliminar'),
     #LIBRO
     #listar y crear libros
-    path('admin/libros/', LibroListCreateAPIView.as_view(), name='admin_libros_listar_crear'),
+    path('v1/admin/libros/', LibroListCreateAPIView.as_view(), name='admin_libros_listar_crear'),
     #listar libro por código | actualizar libro | eliminar libro
-    path('admin/libros/<str:codigo>/', LibroRetrieveUpdateDeleteAPIView.as_view(), name='admin_libros_detalle_editar_eliminar'),
+    path('v1/admin/libros/<str:codigo>/', LibroRetrieveUpdateDeleteAPIView.as_view(), name='admin_libros_detalle_editar_eliminar'),
 
     #* WEB
     #LIBRO
     #listar todos los libros
-    path('web/libros/', LibroListAPIView.as_view(), name='web_libros_listar'),
+    path('v1/web/libros/', LibroListAPIView.as_view(), name='web_libros_listar'),
     #listar libro por código
-    path('web/libros/<str:codigo>/', LibroRetrieveAPIView.as_view(), name='web_libros_detalle'),
+    path('v1/web/libros/<str:codigo>/', LibroRetrieveAPIView.as_view(), name='web_libros_detalle'),
 ]
